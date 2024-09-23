75b3532ec0426725dd9cfbd94a5858c0c0fb9664
HugoW5 <hugo05.wakt@gmail.com>
2024-09-23 14:12:48 +0200

    change int to double

--- Kalkylator/Program.cs
+++ Kalkylator/Program.cs
@@ -1,28 +1,28 @@
 ﻿namespace Kalkylator
-    {
+{
     internal class Program
-        {
+    {
         static void Main(string[] args)
-            {
+        {
             Console.WriteLine("Hello, World!");
-            }
+        }
 
 
-        static int Multiplication(int a, int b)
-            {
+        static double Multiplication(double a, double b)
+        {
             return a * b;
-            }
+        }
 
 
-        static double Division(int a, int b)
-            {
+        static double Division(double a, double b)
+        {
             return a / b;
-            }
+        }
 
 
-        static int Addition(int a, int b)
-            {
+        static double Addition(double a, double b)
+        {
             return a + b;
-            }
         }
     }
+}
