c73f45ac4dfa4c290016733d59bcee2196418102
HugoW5 <hugo05.wakt@gmail.com>
2024-09-23 14:01:47 +0200

    SquareRoot method

--- Kalkylator/Program.cs
+++ Kalkylator/Program.cs
@@ -1,28 +1,33 @@
 ﻿namespace Kalkylator
-    {
+{
     internal class Program
-        {
+    {
         static void Main(string[] args)
-            {
+        {
             Console.WriteLine("Hello, World!");
-            }
+        }
 
 
         static int Multiplication(int a, int b)
-            {
+        {
             return a * b;
-            }
+        }
 
 
         static double Division(int a, int b)
-            {
+        {
             return a / b;
-            }
+        }
 
 
         static int Addition(int a, int b)
-            {
+        {
             return a + b;
-            }
         }
-    }
+
+        static double SquareRoot(int a)
+        {
+            return Math.Sqrt(a);
+        }
+        }
+}
