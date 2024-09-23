47cdc1ec6b0e7b429db84e23faaa73875005eae1
HugoW5 <hugo05.wakt@gmail.com>
2024-09-23 13:57:25 +0200

    Added subtraction method

--- Kalkylator/Program.cs
+++ Kalkylator/Program.cs
@@ -1,28 +1,32 @@
 ﻿namespace Kalkylator
-    {
+{
     internal class Program
-        {
+    {
         static void Main(string[] args)
-            {
+        {
             Console.WriteLine("Hello, World!");
-            }
+        }
 
 
         static int Multiplication(int a, int b)
-            {
+        {
             return a * b;
-            }
+        }
 
 
         static double Division(int a, int b)
-            {
+        {
             return a / b;
-            }
+        }
 
 
         static int Addition(int a, int b)
-            {
+        {
             return a + b;
-            }
+        }
+        static int Subtraction(int a, int b)
+        {
+            return a - b;
         }
     }
+}
